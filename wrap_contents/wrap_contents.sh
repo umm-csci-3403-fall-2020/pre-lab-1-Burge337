2ada6207d84e2b21405e8ac48ad41ccf97119c6d
Ben <burge337@morris.umn.edu>
2020-08-31 00:26:32 -0500

    Add the shabang! and create variables to store the 3 arguments.

---
+++ wrap_contents/wrap_contents.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#The script allows for three arguments: name of the file to be wrapped, the name of the header/footer, the name
+#of the resulting file.
+
+#Name of starting file
+arg_wrap=$1
+#Name of header/footer
+arg_prepend=$2
+#Name of resulting file
+arg_wrapped=$3
