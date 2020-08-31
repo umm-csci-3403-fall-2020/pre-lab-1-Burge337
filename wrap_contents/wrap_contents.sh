e2adc200ca927cbd750e3edab1e4ba4bb5430f04
Ben <burge337@morris.umn.edu>
2020-08-31 18:14:41 -0500

    Finish the wrap_contents.sh script.
    Also created the my_chart.html file in the chart_example directory using said script.

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -3,9 +3,26 @@
 #The script allows for three arguments: name of the file to be wrapped, the name of the header/footer, the name
 #of the resulting file.
 
+#Variables
 #Name of starting file
-arg_wrap=$1
-#Name of header/footer
+input_text=$1
+
+#Prefix of header/footer
 arg_prepend=$2
+
+#Full header file name
+header=$arg_prepend
+header+="_header.html"
+
+#Full footer file name
+footer=$arg_prepend
+footer+="_footer.html"
+
 #Name of resulting file
-arg_wrapped=$3
+output_file=$3
+
+#Create an empty resulting file with our desired name
+touch "$output_file"
+
+#Crunch all of our text together into the output file
+cat $header "$input_text" $footer > "$output_file" 
